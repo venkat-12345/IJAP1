baf1a792d71fc6c9d860a213c0175d2c3e3b224e
venkat-12345 <65700868+venkat-12345@users.noreply.github.com>
2022-12-31 12:02:50 +0530

    Delete IJAP/EmployeePostApiSvc directory

--- IJAP/EmployeePostApiSvc/EmployeePostApiSvc/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace EmployeePostApiSvc.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- IJAP/EmployeePostApiSvc/EmployeePostApiSvc/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace EmployeePostApiSvc.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
